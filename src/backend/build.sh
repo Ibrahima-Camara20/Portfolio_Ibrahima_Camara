b7bacdf9c585f40e707b084e2610a447169ffb05
ibrahima camara <ibrahima.camara@etu.univ-cotedazur.fr>
2026-02-19 11:09:55 +0100

    temp: auto-create superuser from env vars on build

--- src/backend/build.sh
+++ src/backend/build.sh
@@ -4,3 +4,22 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Création/réinitialisation du superuser via variables d'environnement
+python manage.py shell -c "
+from django.contrib.auth.models import User
+import os
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', '')
+if not password:
+    print('DJANGO_SUPERUSER_PASSWORD not set, skipping.')
+elif User.objects.filter(username=username).exists():
+    u = User.objects.get(username=username)
+    u.set_password(password)
+    u.save()
+    print(f'Password updated for {username}')
+else:
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print(f'Superuser {username} created')
+"
